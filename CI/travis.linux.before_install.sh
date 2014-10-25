5de562e201b979fc7e0eb9c314599399f3a3a69e
me <noemail@nowhere.com>
2014-10-24 22:24:36 -0600

    More of this again

--- CI/travis.linux.before_install.sh
+++ CI/travis.linux.before_install.sh
@@ -5,5 +5,6 @@ sudo apt-add-repository ppa:ubuntu-sdk-team/ppa -y
 #sudo apt-add-repository ppa:kalakris/cmake -y
 # newer GCC version, 4.7
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo add-apt-repository ppa:boost-latest/ppa -y
+#sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo apt-get update
+sudo apt-cache search qt5
