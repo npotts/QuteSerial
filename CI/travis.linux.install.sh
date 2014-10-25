c588408a922b0649bcfd4009a136019685cd9ec9
me <noemail@nowhere.com>
2014-10-24 22:20:42 -0600

    Minor Improvements

--- CI/travis.linux.install.sh
+++ CI/travis.linux.install.sh
@@ -3,15 +3,7 @@
 set -ev
 sudo apt-get install \
   build-essential \
-  qt5-default qtmultimedia5-dev qttools5-dev \
-  libhunspell-dev \
-  lua5.1 liblua5.1-0-dev \
-  libpcre3-dev \
-  libboost1.55-dev\
-  zlib1g-dbg zlib1g-dev \
-  libzip-dev \
-  libpulse-dev \
-  cmake \
+  qt5-default qtmultimedia5-dev qttools5-dev qttools5-serialport\
   gcc-4.7 \
   g++-4.7
 sudo update-alternatives --remove gcc /usr/bin/gcc-4.6
